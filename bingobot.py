43bc036a085d6df9724368996f21043b3a0402d8
Trash King <reedtakesemail@gmail.com>
2020-10-29 14:19:17 -0700

    init

--- bingobot.py
+++ bingobot.py
@@ -1,15 +1,48 @@
 import os
-import random
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from googletrans import Translator
+from textgenrnn import textgenrnn
+
 translator = Translator()
 
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+with open("C:\\Users\\Reed\\Desktop\\bingo bot\\token.txt", "r") as f:
+    key = f.readlines()[0]
+    
+token = key
+#to be re-enabled upon bot deployment
+#load_dotenv()
+#token = os.getenv('DISCORD_TOKEN') 
+
 client = discord.Client()
-            
+
+def neuralQuote(num):
+    if int(num) < 21:
+        textgen_2 = textgenrnn('textgenrnn_weights.hdf5')
+        value = textgen_2.generate(int(num), return_as_list=True) 
+        return value
+    else:
+        print("in else")
+
+    
+def trans(msg):
+    lang = msg.split()[0]
+    text = " ".join(msg.split()[1:])
+    print(text)
+    print(lang)
+    try:
+        translated = translator.translate(text, dest=lang)
+        return translated.text
+    
+    except ValueError as e:
+        return e
+    
+    except:
+        unknown = "unexpected error has occured."
+        return unknown    
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord')
@@ -17,18 +50,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if '!translate' in message.content.lower():
-        await message.channel.send(ctx.message.content)
+    if '!t' in message.content.lower():
+        text = message.content
+        text = text.replace("!t","")
+        translated = trans(text)
+        await message.channel.send(translated)
 
-
-    if 'fav that' in message.content.lower():
-
-        await message.channel.send('Possum pic added to favorites!')
-        
-    if 'post fav possums' in message.content.lower():
-        await message.channel.send(file=file)
-
-
+    if '!q' in message.content.lower():
+        text = message.content
+        text = text.replace("!q","")
+        text = text.strip(" ")
+        quoteRequest = neuralQuote(text)
+        if len(quoteRequest) >= 1:
+            for quote in quoteRequest:
+                await message.channel.send(quote)
+        else:
+            await message.channel.send(quoteRequest)
         
 client.run(token)
-
